fc5e0ac30587b7959b0d8c4c57e8e9ef9c9a98cd
nocturnuscoder <nocturnuscoder@pm.me>
2025-06-25 13:53:20 +0300

    Add contracts directory

---
+++ js-scripts/generateProof.ts
@@ -0,0 +1,66 @@
+import { Barretenberg, Fr, UltraHonkBackend } from "@aztec/bb.js";
+import { ethers } from "ethers";
+import { Noir } from "@noir-lang/noir_js";
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const circuitPath = path.resolve(__dirname, "../circuits/target/panagram.json");
+const circuit = JSON.parse(fs.readFileSync(circuitPath, 'utf8'));
+
+export default async function generateProof() {
+  // Initialize Barretenberg
+  const bb = await Barretenberg.new();
+
+  // Get the inputs from the args
+  const inputsArray = process.argv.slice(2);
+
+  try {
+    const noir = new Noir(circuit);
+    const honk = new UltraHonkBackend(circuit.bytecode, { threads: 1 });
+
+    const input = {
+      // Private inputs
+      guess_hash: inputsArray[0],
+      
+      // Public inputs
+      answer_double_hash: inputsArray[1],
+      address: inputsArray[2]
+    };
+
+    const { witness } = await noir.execute(input);
+
+    const originalLog = console.log; // Save original
+    // Override to silence all logs
+    console.log = () => {};
+
+    const { proof, publicInputs } = await honk.generateProof(witness, { keccak: true });
+    const offChainProof = await honk.generateProof(witness);
+    const isValid = await honk.verifyProof(offChainProof);
+    // Restore original console.log
+    console.log = originalLog;
+
+    const res = ethers.AbiCoder.defaultAbiCoder().encode(
+        ["bytes", "bytes32[]"],
+        [proof, publicInputs]
+      );
+    return res;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+(async () => {
+    generateProof()
+    .then((res) => {
+      process.stdout.write(res);
+      process.exit(0);
+    })
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+})();
